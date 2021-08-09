681126e5544a60047f56e28f3a4ba53e08dd85fa
gu1233 <gde102@naver.com>
2021-08-09 17:47:59 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'GuDaeun',
+    image: 'https://heropy.blog/css/images/logo.png',
+    email: 'gde102@naver.com',
+    phone: '+82-10-4930-9510'
+  })
+}
